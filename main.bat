88d79a8535a1be129e20a917a9c76d8f66788676
FGabi01 <46404350+FGabi01@users.noreply.github.com>
2020-03-24 22:53:01 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,6 +1,6 @@
 powershell -Command "Invoke-WebRequest http://art-exlibris.net/images/large/exlibris_21693.jpg -OutFile %temp%\wp.jpg"
 timeout 1
-screenshot.exe %tmp%img.png
+screenshot.exe %temp%\img.png
 start %tmp%\img.png
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /f /d %tmp%\wp.jpg
