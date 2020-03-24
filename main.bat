95def960b274a7bcec409d13bc74c669d46a43d5
FGabi01 <46404350+FGabi01@users.noreply.github.com>
2020-03-24 22:50:45 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,11 +1,11 @@
-powershell -Command "Invoke-WebRequest http://art-exlibris.net/images/large/exlibris_21693.jpg -OutFile %temp%\img.jpg"
+powershell -Command "Invoke-WebRequest http://art-exlibris.net/images/large/exlibris_21693.jpg -OutFile %temp%\wp.jpg"
 timeout 1
 screenshot.exe %tmp%img.png
-start %tmp%\img.jpg
-timeout 1
-taskkill /IM Microsoft.Photos.exe /f
+start %tmp%\img.png
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /f /d %tmp%\img.png
+reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /f /d %tmp%\wp.jpg
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
+timeout 1
+taskkill /IM Microsoft.Photos.exe /f
 exit
