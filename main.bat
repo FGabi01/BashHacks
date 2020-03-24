dba223edb31a49c9c2efe57c076b3d0a1e9849a9
FGabi01 <46404350+FGabi01@users.noreply.github.com>
2020-03-24 20:54:29 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 powershell -Command "Invoke-WebRequest https://doc.lagout.org/security/The-Hacker-Playbook-Practical-Guide-To-Penetration-Testing-2014.pdf -OutFile %temp%\book.pdf"
-timeout 1
+timeout 10
 start  chrome.exe "%temp%\book.pdf"
 timeout 5
 taskkill /IM chrome.exe -F
